d43b678adee1b0d5f36fe9ffa60b3e28c8088eac
ItsChamboDeveloper <imchambo503@gmail.com>
2026-08-12 20:35:34 -0900

    Actualizar app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+import { config } from "./config.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import express from "express";
@@ -19,6 +20,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173", 
   "http://localhost:5174",
+  "http://127.0.0.1:5173",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
